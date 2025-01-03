3458b1800701ce032277f5f64a6970b16b783270
VISHESH SACHAN <visheshsachan021@gmail.com>
2025-01-03 15:32:21 +0530

    Update blogs.ts

--- constants/blogs.ts
+++ constants/blogs.ts
@@ -7,7 +7,13 @@ export interface Blog {
 }
 
 export const blogs: Blog[] = [
-    
+    {
+        title: "Outing Pass System",
+        description: "An Out Pass genration system for Hostel students.",
+        date: "03-01-2024",
+        link: "https://amusing-sushi-28f.notion.site/Outing-Pass-System-8e7a5c6e2d564c66ac3345f38d2c0f5f",
+        isProjectDoc: true
+    },
 ];
 
 // {
